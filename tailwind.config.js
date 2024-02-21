d7a0aea778af10f7e094dc7b0f6ab85d5bce2911
M-K-07 <muralikrishnasahu271@gmail.com>
2024-02-21 16:30:11 +0530

    Project Created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      code: ['Sixtyfour', 'sans-serif'],
+      protest:['Protest Guerrilla', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: ['tailwindcss'],
+}
+
